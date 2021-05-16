8c0ed1aea96f5b601c0edda0a1299af6a8871a8e
ionolaru <spideiron@live.it>
2021-05-16 21:14:22 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:lesson1/my_app.dart';
+
+void main() {
+  final MaterialApp app = MaterialApp(home: MyApp());
+  runApp(app);
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color backgroundColor = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    final widget = Scaffold(
+        backgroundColor: backgroundColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Alice'),
+              Text('Ion'),
+              rowName(),
+              button(),
+            ],
+          ),
+        ));
+    return widget;
+  }
+
+  Widget button() {
+    return ElevatedButton(onPressed: onButtonPressed, child: Text('BUTTON'));
+  }
+
+  void onButtonPressed() {
+    if (backgroundColor == Colors.red) {
+      backgroundColor = Colors.blue;
+    } else {
+      backgroundColor = Colors.red;
+    }
+    setState(() {});
+  }
+
+  Row rowName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Alice'),
+        SizedBox(width: 20),
+        Text('Ion'),
+        SizedBox(width: 20),
+        Text('Ion'),
+        SizedBox(width: 20),
+        Text('Ion'),
+      ],
+    );
+  }
+}
